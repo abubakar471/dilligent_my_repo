46ad0d7e33fc031ddbb2dd4524b51f891f2a9cd1
abubakar471 <abdurjoy2001@gmail.com>
2023-10-15 13:10:03 +0600

    loader added

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,11 +3,6 @@ import { Helmet } from "react-helmet";
 const Loader = () => {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Loading...</title>
-      </Helmet>
-
       <div className="h-screen flex items-center justify-center">
         <div className="text-4xl font-bold text-gray-700 2 border-spacing-10 p-4 flex flex-col items-center justify-center">
           <img src="/assets/diligent_loader.jpg" alt="diligent_loader" />
